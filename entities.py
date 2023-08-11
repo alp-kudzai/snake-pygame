14ea58feeffed4a641862013520cbb67414d980d
Kudzai <alpha.kudzai@gmail.com>
2023-08-11 11:06:30 +0200

    working game, upgraded to pygame-ce 2.3.1

--- entities.py
+++ entities.py
@@ -1,26 +1,92 @@
 import pygame as pg
 
 SPEED = 10
+class Colli_Object:
+    def __init__(self, size, pos, screen, color):
+        self.width = size[0]
+        self.height = size[1]
+        self.pos = pos
+        self.screen = screen
+        self.color = color
+        self.object = pg.Rect(self.pos[0], self.pos[1], self.width, self.height)
+
+    def move(self, coords):
+        self.pos[0] += coords[0]
+        self.pos[1] += coords[1]
+        self.object = pg.Rect(self.pos[0], self.pos[1], self.width, self.height)
+
+    def render(self):
+        self.object = pg.Rect(self.pos[0], self.pos[1], self.width, self.height)
+        pg.draw.rect(self.screen, self.color, self.object)
+
+    def __repr__(self):
+        return f'Current Pos: {self.pos}'
+
+
+
 class Snake:
     def __init__(self, dim, game):
         self.size = 10
-        self.body = []
         self.game = game
         self.last_direction = 4
-        self.pos = [(dim[0]/2), (dim[1]/2)]
+        self.pos = [(dim[0]//2), (dim[1]//2)]
         self.snake = pg.Rect(self.pos[0], self.pos[1], self.size, self.size)
+        self.body = []
+        self.body.append(self.snake)
+        # self.body.append(Colli_Object([10,10], [self.pos[0] - 10, self.pos[1]], self.game.screen, 'grey'))
+        # self.body.append(Colli_Object([10,10], [self.pos[0] - 20, self.pos[1]], self.game.screen, 'grey'))
+        self.length = 1
+        self.color = 'grey'
+        self.path = []
+        self.sizeLimit = 100
 
     def getDirection(self, pressed_y, pressed_x):
-        if pressed_x[0]:
+        if pressed_x[0] and self.last_direction != 4:
             self.last_direction = 8
-        if pressed_x[1]:
+        if pressed_x[1] and self.last_direction != 8:
             self.last_direction = 4
-        if pressed_y[0]:
+        if pressed_y[0] and self.last_direction != 1:
             self.last_direction = 2
-        if pressed_y[1]:
+        if pressed_y[1] and self.last_direction != 2:
             self.last_direction = 1
-    
+
+    def eat(self):
+        size = self.size
+        if self.length == 1:
+            match self.last_direction:
+                case 8:
+                    newpos =  [(self.pos[0] + SPEED), self.pos[1]]
+                case 4:
+                    newpos =  [(self.pos[0] - SPEED), self.pos[1]]
+                case 2:
+                    newpos = [ self.pos[0], (self.pos[1] + SPEED)]
+                case 1:
+                    newpos = [ self.pos[0], (self.pos[1] - SPEED)]
+        else:
+            lastpos = self.body[-1].pos
+            match self.last_direction:
+                case 8:
+                    newpos =  [(lastpos[0] + SPEED), lastpos[1]]
+                case 4:
+                    newpos =  [(lastpos[0] - SPEED), lastpos[1]]
+                case 2:
+                    newpos = [ lastpos[0], (lastpos[1] + SPEED)]
+                case 1:
+                    newpos = [ lastpos[0], (lastpos[1] - SPEED)]
+
+        seg = Colli_Object([self.size, self.size], newpos, self.game.screen, self.color)
+        self.body.append(seg)
+        self.length += 1
+        #print(self.body)
+
+    def moveBody(self):
+        path = self.path[::-1]
+        for i, s in enumerate(self.body[1:]):
+            s.pos = path[i]
+        
+        
     def move(self):
+        last_pos = self.pos[:]
         match self.last_direction:
             case 8:
                 self.pos[0] -= SPEED
@@ -30,7 +96,24 @@ class Snake:
                 self.pos[1] -= SPEED
             case 1:
                 self.pos[1] += SPEED
+        if self.length > 1:
+            self.moveBody()
+        #print(self.path)
+        trail = self.pos[:]
+        self.path.append(trail)
+        self.snake = pg.Rect(self.pos[0], self.pos[1], self.size, self.size)
+        if len(self.path) >= self.sizeLimit:
+            self.path.pop(0)
+
+        
+        # for s in self.body[1:]:
+        #     temp = s.pos
+        #     s.move(last_pos)
+        #     last_pos = temp
+    
 
     def render(self):
         self.snake = pg.Rect(self.pos[0], self.pos[1], self.size, self.size)
-        pg.draw.rect(self.game.screen, 'grey', self.snake)
+        pg.draw.rect(self.game.screen, self.color, self.snake)
+        for seg in self.body[1:]:
+            seg.render()
--- game.py
+++ game.py
@@ -1,24 +1,93 @@
 import pygame as pg
 import sys
-from entities import Snake
-
+from entities import Snake, Colli_Object
+import random
+import time
 class Game:
     def __init__(self):
         pg.init()
-        self.FPS = 10
         self.DIMENSIONS = [500,500]
+        self.screen = pg.display.set_mode(self.DIMENSIONS)
+        self.Snake = Snake(self.DIMENSIONS, self)
+        self.FPS = 10
+        self.SCALE = self.Snake.size*2
+        
+        self.BX_WIDTH = self.DIMENSIONS[0]
+        self.BX_HEIGHT = (self.Snake.size*2)
+        self.BY_WIDTH = self.BX_HEIGHT
+        self.BY_HEIGHT = self.BX_HEIGHT
         self.clock = pg.time.Clock()
         pg.display.set_caption('snake game')
-        self.screen = pg.display.set_mode(self.DIMENSIONS)
+        
         self.font = pg.font.SysFont('Verdana', 20)
         self.fpsText = self.font.render(str(round(self.clock.get_fps())), True, (0,0,0))
-        self.Snake = Snake(self.DIMENSIONS, self)
+        self.scoreText = self.font.render(str(self.Snake.length-1), True, 'red')
+        
         self.x_keys_pressed = [False, False]
         self.y_keys_pressed = [False, False]
+        self.collision_objects = []
+        self.food = []
+
+    def endGame(self):
+        self.Snake = Snake(self.DIMENSIONS, self)
+        #time.sleep(0.5)
+
+    def checkCollisions(self):
+        for ob in self.collision_objects:
+            if self.Snake.snake.colliderect(ob.object):
+                self.endGame()
+        #check if food was eaten
+        if self.Snake.snake.colliderect(self.food[0]):
+            self.Snake.eat()
+            self.food.clear()
+        #check if snake collided with self
+        for seg in self.Snake.body[2:]:
+            if self.Snake.snake.colliderect(seg.object):
+                self.endGame()
+
+    def spawnFood(self):
+        if len(self.food) == 0:
+            pos_x = random.randrange((self.SCALE*2), (self.DIMENSIONS[1] - self.SCALE*2),10)
+            pos_y = random.randrange((self.SCALE*2), (self.DIMENSIONS[1] - self.SCALE*2),10)
+            food_rect = pg.Rect(pos_x, pos_y, 10, 10)
+            self.food.append(food_rect)
+            #print([pos_x, pos_y])
+
+    def renderFood(self):
+        pg.draw.rect(self.screen, 'yellow', self.food[0])
+
+
+    def createBorders(self):
+        left_ver_border = [0,self.BY_HEIGHT]
+        right_ver_border = [(self.BX_WIDTH - self.BX_HEIGHT), self.BX_HEIGHT]
+
+        top_hor_border = [0, 0]
+        bott_hor_border = [0, (self.DIMENSIONS[1] - self.BX_HEIGHT)]
+
+        top = Colli_Object([self.DIMENSIONS[0], self.BX_HEIGHT], top_hor_border, self.screen, 'grey')
+        bottom = Colli_Object([self.DIMENSIONS[0], self.BX_HEIGHT], bott_hor_border, self.screen, 'grey')
+        #left = pg.Rect(left_ver_border[0], left_ver_border[1], self.BX_HEIGHT, (self.DIMENSIONS[0] - (self.BX_HEIGHT) * 2))
+        left = Colli_Object([self.BX_HEIGHT, (self.DIMENSIONS[0] - (self.BX_HEIGHT) * 2)], [left_ver_border[0], left_ver_border[1]], self.screen, 'grey')
+        #right = pg.Rect(right_ver_border[0], right_ver_border[1], self.BX_HEIGHT, (self.DIMENSIONS[0] - (self.BX_HEIGHT) * 2))
+        right = Colli_Object([self.BX_HEIGHT, (self.DIMENSIONS[0] - (self.BX_HEIGHT) * 2)], [right_ver_border[0], right_ver_border[1]], self.screen, 'grey')
+        self.collision_objects.append(top)
+        self.collision_objects.append(bottom)
+        self.collision_objects.append(left)
+        self.collision_objects.append(right)
+        #pg.draw.rect(self.screen, 'red', left)
+        
+        #pg.draw.rect(self.screen, 'red', right)
+
+    def renderBorder(self):
+        for b in self.collision_objects:
+            b.render()
 
     def renderFPStext(self):
         self.fpsText = self.font.render(str(round(self.clock.get_fps(),2)), True, (127,127,127))
         self.screen.blit(self.fpsText, (0,0))
+        self.scoreText = self.font.render(str(self.Snake.length-1), True, 'red')
+        self.screen.blit(self.scoreText, (self.DIMENSIONS[0] - self.SCALE*3, self.SCALE))
+
 
     def processInput(self):
         for event in pg.event.get():
@@ -50,9 +119,6 @@ class Game:
                 if event.key == pg.K_RIGHT:
                     self.x_keys_pressed[1] = False
                 
-
-
-
     def run(self):
         running = True
         while running:
@@ -60,15 +126,25 @@ class Game:
             self.processInput()
 
             self.screen.fill('black')
-            self.Snake.getDirection(self.y_keys_pressed, self.x_keys_pressed)
+            self.spawnFood()
+            self.renderFood()
             self.Snake.move()
+            self.checkCollisions()
+            self.Snake.getDirection(self.y_keys_pressed, self.x_keys_pressed)
 
             #Render here
+            
+            self.renderBorder()
             self.renderFPStext()
+            
             self.Snake.render()
+            
+            
             pg.display.flip()
             self.clock.tick(self.FPS)
 
-Game().run()
+g = Game()
+g.createBorders()
+g.run()
 
 
